b044b46501bb9504f756bd9d8cae8583289b3005
sakshyasinha <142413749+sakshyasinha@users.noreply.github.com>
2025-02-05 20:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,20 +12,25 @@ const messages = [
 ];
 
 let messageIndex = 0;
+
 const gifs = [
-    'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTdzMGlwaDBsYXpmdXFsbWk5bXM5d3c0NHR1dWl4aHZjdXF3M2U3NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3og0IG0skAiznZQLde/giphy.gif'];
+    'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTdzMGlwaDBsYXpmdXFsbWk5bXM5d3c0NHR1dWl4aHZjdXF3M2U3NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3og0IG0skAiznZQLde/giphy.gif' 
+];
 
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    const gifImg = document.querySelector('.gif-container');
+    const gifImg = document.querySelector('.gif-container img'); // Selecting the <img> inside the .gif-container
+
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
+
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
+
     gifImg.src = gifs[messageIndex % gifs.length];
 }
 
 function handleYesClick() {
-    window.location.href = "yes_page.html";
+    window.location.href = "yes_page.html"; 
 }
